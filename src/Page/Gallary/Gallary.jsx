8d2fe0706d5c8495ccff8d54bf30ce9f48a7e00d
Md.Tarikul Islam Juel <md.tarikulislamjuel@gmail.com>
2022-09-01 18:14:31 +0600

    8

--- src/Page/Gallary/Gallary.jsx
+++ src/Page/Gallary/Gallary.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Cards from '../../Components/Cards/Cards';
 import gallaryImages from '../../gallaryImages';
+import './Gallary.css';
 
 export default function Gallary() {
     return (
         <>
             <section className="main-body ">
+                <div>
+                    <h1 className="gallery-text">Gallery</h1>
+                </div>
                 <Cards Images={gallaryImages} />
             </section>
         </>
--- src/Page/Projects/Projects.jsx
+++ src/Page/Projects/Projects.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Cards from '../../Components/Cards/Cards';
 import projectImages from '../../projectImages';
-
 import './Projects.css';
 
 export default function Projects() {
     return (
         <>
             <section className="main-body ">
+                <div>
+                    <h1 className="project-text">Projects</h1>
+                </div>
                 <Cards Images={projectImages} />
             </section>
         </>
