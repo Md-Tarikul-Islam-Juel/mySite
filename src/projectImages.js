a5c2a1e7d17cf229c935512803ede262be7bba8c
Md.Tarikul Islam Juel <md.tarikulislamjuel@gmail.com>
2022-09-01 17:54:14 +0600

    7

--- src/projectImages.js
+++ src/projectImages.js
@@ -1,36 +1,36 @@
 const projectImages = [
     {
-        image: 'img/Project/1.jpeg',
+        image: 'img/Project/1.jpg',
     },
     {
-        image: 'img/Project/2.jpeg',
+        image: 'img/Project/2.jpg',
     },
     {
-        image: 'img/Project/3.jpeg',
+        image: 'img/Project/3.jpg',
     },
     {
-        image: 'img/Project/4.jpeg',
+        image: 'img/Project/4.jpg',
     },
     {
-        image: 'img/Project/5.jpeg',
+        image: 'img/Project/5.jpg',
     },
     {
-        image: 'img/Project/6.jpeg',
+        image: 'img/Project/6.jpg',
     },
     {
-        image: 'img/Project/7.jpeg',
+        image: 'img/Project/7.jpg',
     },
     {
-        image: 'img/Project/8.jpeg',
+        image: 'img/Project/8.jpg',
     },
     {
-        image: 'img/Project/9.jpeg',
+        image: 'img/Project/9.jpg',
     },
     {
         image: 'img/Project/10.jpg',
     },
     {
-        image: 'img/Project/11.jpeg',
+        image: 'img/Project/11.jpg',
     },
 ];
 
