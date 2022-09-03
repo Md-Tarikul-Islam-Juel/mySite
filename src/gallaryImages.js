2a7177e856c851b96ffb545983a4734af8fecdc2
Md.Tarikul Islam Juel <md.tarikulislamjuel@gmail.com>
2022-09-03 12:04:12 +0600

    11

--- src/gallaryImages.js
+++ src/gallaryImages.js
@@ -1,78 +1,78 @@
 const gallaryImages = [
     {
-        image: 'img/Gallary/1.jpg',
+        image: 'img/Gallary/1.webp',
     },
     {
-        image: 'img/Gallary/2.jpg',
+        image: 'img/Gallary/2.webp',
     },
     {
-        image: 'img/Gallary/3.jpg',
+        image: 'img/Gallary/3.webp',
     },
     {
-        image: 'img/Gallary/4.jpg',
+        image: 'img/Gallary/4.webp',
     },
     {
-        image: 'img/Gallary/5.jpg',
+        image: 'img/Gallary/5.webp',
     },
     {
-        image: 'img/Gallary/6.jpg',
+        image: 'img/Gallary/6.webp',
     },
     {
-        image: 'img/Gallary/7.jpg',
+        image: 'img/Gallary/7.webp',
     },
     {
-        image: 'img/Gallary/8.jpg',
+        image: 'img/Gallary/8.webp',
     },
     {
-        image: 'img/Gallary/9.jpg',
+        image: 'img/Gallary/9.webp',
     },
     {
-        image: 'img/Gallary/10.jpg',
+        image: 'img/Gallary/10.webp',
     },
     {
-        image: 'img/Gallary/11.jpg',
+        image: 'img/Gallary/11.webp',
     },
     {
-        image: 'img/Gallary/12.jpg',
+        image: 'img/Gallary/12.webp',
     },
     {
-        image: 'img/Gallary/13.jpg',
+        image: 'img/Gallary/13.webp',
     },
     {
-        image: 'img/Gallary/14.jpg',
+        image: 'img/Gallary/14.webp',
     },
     {
-        image: 'img/Gallary/15.jpg',
+        image: 'img/Gallary/15.webp',
     },
     {
-        image: 'img/Gallary/16.jpg',
+        image: 'img/Gallary/16.webp',
     },
     {
-        image: 'img/Gallary/17.jpg',
+        image: 'img/Gallary/17.webp',
     },
     {
-        image: 'img/Gallary/18.jpg',
+        image: 'img/Gallary/18.webp',
     },
     {
-        image: 'img/Gallary/19.jpg',
+        image: 'img/Gallary/19.webp',
     },
     {
-        image: 'img/Gallary/20.jpg',
+        image: 'img/Gallary/20.webp',
     },
     {
-        image: 'img/Gallary/21.jpg',
+        image: 'img/Gallary/21.webp',
     },
     {
-        image: 'img/Gallary/22.jpg',
+        image: 'img/Gallary/22.webp',
     },
     {
-        image: 'img/Gallary/23.jpg',
+        image: 'img/Gallary/23.webp',
     },
     {
-        image: 'img/Gallary/24.jpg',
+        image: 'img/Gallary/24.webp',
     },
     {
-        image: 'img/Gallary/25.jpg',
+        image: 'img/Gallary/25.webp',
     },
 ];
 
--- src/projectImages.js
+++ src/projectImages.js
@@ -1,36 +1,36 @@
 const projectImages = [
     {
-        image: 'img/Project/1.jpg',
+        image: 'img/Project/1.webp',
     },
     {
-        image: 'img/Project/2.jpg',
+        image: 'img/Project/2.webp',
     },
     {
-        image: 'img/Project/3.jpg',
+        image: 'img/Project/3.webp',
     },
     {
-        image: 'img/Project/4.jpg',
+        image: 'img/Project/4.webp',
     },
     {
-        image: 'img/Project/5.jpg',
+        image: 'img/Project/5.webp',
     },
     {
-        image: 'img/Project/6.jpg',
+        image: 'img/Project/6.webp',
     },
     {
-        image: 'img/Project/7.jpg',
+        image: 'img/Project/7.webp',
     },
     {
-        image: 'img/Project/8.jpg',
+        image: 'img/Project/8.webp',
     },
     {
-        image: 'img/Project/9.jpg',
+        image: 'img/Project/9.webp',
     },
     {
-        image: 'img/Project/10.jpg',
+        image: 'img/Project/10.webp',
     },
     {
-        image: 'img/Project/11.jpg',
+        image: 'img/Project/11.webp',
     },
 ];
 
