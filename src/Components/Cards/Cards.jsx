7ee58278e058b988a3516a9a7dd5f7917a3b7ee5
Md.Tarikul Islam Juel <md.tarikulislamjuel@gmail.com>
2022-09-03 11:03:50 +0600

    10

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,17 +1,29 @@
 /* eslint array-callback-return: "error" */
 import React from 'react';
-import Card from '../Card/Card';
+// import Card from '../Card/Card';
+import { LazyLoadImage, trackWindowScroll } from 'react-lazy-load-image-component';
 import './Cards.css';
 
-export default function Cards({ Images }) {
+function Cards({ Images, scrollPosition }) {
     return (
         <>
             <div className="container">
                 <div className="row cards-row-1">
                     {Images.map((item) => (
-                        <div className="CARD">
-                            {' '}
-                            <Card img={item.image} />{' '}
+                        <div className="CARD card-box">
+                            {/* <Card img={item.image} /> */}
+                            <LazyLoadImage
+                                key={item.image.key}
+                                alt={item.image.alt}
+                                height={item.image.height}
+                                // Make sure to pass down the scrollPosition,
+                                // this will be used by the component to know
+                                // whether it must track the scroll position or not
+                                scrollPosition={scrollPosition}
+                                src={item.image}
+                                width={item.image.width}
+                                className="card-img"
+                            />
                         </div>
                     ))}
                 </div>
@@ -19,3 +31,5 @@ export default function Cards({ Images }) {
         </>
     );
 }
+
+export default trackWindowScroll(Cards);
